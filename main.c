916e8673f557ff055f43cac39af59350fc232013
Grebelnikov_Ivan <grivmih@gmail.com>
2026-04-26 17:32:56 +0700

    construction of functions as in the problem

--- main.c
+++ main.c
@@ -3,39 +3,34 @@
 #include <string.h>
 #include <stdlib.h>
 
-unsigned char* convertStrToLongBv(char* str, size_t* cells);
-char* convertBvToStr(unsigned char* vec, size_t sz);
-unsigned char* addBitVectors(unsigned char* vec, unsigned char* vec2, size_t cells, size_t cells2);
-unsigned char* multiplyBitVectors(unsigned char* vec, unsigned char* vec2, size_t cells, size_t cells2);
-unsigned char* xorBitVectors(unsigned char* vec, unsigned char* vec2, size_t cells, size_t cells2);
-int set_bit_1(unsigned char *vec, size_t len, size_t k);
-int set_bit_0(unsigned char *vec, size_t len, size_t k);
-unsigned char* inversBitVector(unsigned char* vec, size_t len);
-int shift_left_bit_vector(unsigned char *vec, size_t len, size_t k);
-int shift_right_bit_vector(unsigned char *vec, size_t len, size_t k);
+unsigned char* convertStrToLongBv(char* str, size_t* bits);
+char* convertBvToStr(unsigned char* vec, size_t bits);
+unsigned char* logSum(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB);
+unsigned char* logMul(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB);
+unsigned char* sumMod2(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB);
+void set1(unsigned char *vec, size_t bits, size_t bit);
+void set0(unsigned char *vec, size_t bits, size_t bit);
+void inversion(unsigned char *vec, size_t bits);
+void shiftLeft(unsigned char *vec, size_t bits, size_t k);
+void shiftRight(unsigned char *vec, size_t bits, size_t k);
 
 int main() {
     char* str1 = NULL;
     char* str2 = NULL;
     str1 = "10011000";
     str2 = "00011010";
-    //char* str1 = "00000000";
 
-
-    size_t cells1 = 0, cells2 = 0;
-    size_t len1 = strlen(str1);
-    size_t len2 = strlen(str2);
-    cells1 = ((len1 - 1) / 8) + 1;
-    cells2 = ((len2 - 1) / 8) + 1;
+    size_t bits1 = strlen(str1);
+    size_t bits2 = strlen(str2);
 
     // Конвертация строк в битовые векторы
-    unsigned char* vec1 = convertStrToLongBv(str1, &cells1);
+    unsigned char* vec1 = convertStrToLongBv(str1, &bits1);
     if (vec1 == NULL) {
         printf("error11");
         return 1;
     }
 
-    unsigned char* vec2 = convertStrToLongBv(str2, &cells2);
+    unsigned char* vec2 = convertStrToLongBv(str2, &bits2);
     if (vec2 == NULL) {
         printf("error12");
         free(vec1);
@@ -43,7 +38,7 @@ int main() {
     }
 
     // исходные векторы
-    char* vec1_str = convertBvToStr(vec1, cells1);
+    char* vec1_str = convertBvToStr(vec1, bits1);
     if (vec1_str == NULL) {
         printf("error2");
         free(vec1);
@@ -51,7 +46,7 @@ int main() {
         return 1;
     }
 
-    char* vec2_str = convertBvToStr(vec2, cells2);
+    char* vec2_str = convertBvToStr(vec2, bits2);
     if (vec2_str == NULL) {
         printf("error2");
         free(vec1_str);
@@ -67,14 +62,14 @@ int main() {
     printf("\n");
 
     // +
-    unsigned char* add_res = addBitVectors(vec1, vec2, cells1, cells2);
+    unsigned char* add_res = logSum(vec1, bits1, vec2, bits2);
     if (add_res == NULL) {
         printf("error3");
         free(vec1);
         free(vec2);
         return 1;
     }
-    char* add_str = convertBvToStr(add_res, cells1);
+    char* add_str = convertBvToStr(add_res, bits1);
     if (add_str == NULL) {
         printf("error2");
         free(add_res);
@@ -82,19 +77,19 @@ int main() {
         free(vec2);
         return 1;
     }
-    printf("+    %s\n", add_str);
+    printf("logSum  %s\n", add_str);
     free(add_str);
     free(add_res);
 
     // *
-    unsigned char* mul_res = multiplyBitVectors(vec1, vec2, cells1, cells2);
+    unsigned char* mul_res = logMul(vec1, bits1, vec2, bits2);
     if (mul_res == NULL) {
         printf("error3");
         free(vec1);
         free(vec2);
         return 1;
     }
-    char* mul_str = convertBvToStr(mul_res, cells1);
+    char* mul_str = convertBvToStr(mul_res, bits1);
     if (mul_str == NULL) {
         printf("error2");
         free(mul_res);
@@ -102,19 +97,19 @@ int main() {
         free(vec2);
         return 1;
     }
-    printf("*    %s\n", mul_str);
+    printf("logMul  %s\n", mul_str);
     free(mul_str);
     free(mul_res);
 
     // xor
-    unsigned char* xor_res = xorBitVectors(vec1, vec2, cells1, cells2);
+    unsigned char* xor_res = sumMod2(vec1, bits1, vec2, bits2);
     if (xor_res == NULL) {
         printf("error3");
         free(vec1);
         free(vec2);
         return 1;
     }
-    char* xor_str = convertBvToStr(xor_res, cells1);
+    char* xor_str = convertBvToStr(xor_res, bits1);
     if (xor_str == NULL) {
         printf("error2");
         free(xor_res);
@@ -122,66 +117,48 @@ int main() {
         free(vec2);
         return 1;
     }
-    printf("xor  %s\n", xor_str);
+    printf("sumMod2 %s\n", xor_str);
     free(xor_str);
     free(xor_res);
 
-    // ~
-    unsigned char* inv_res = inversBitVector(vec1, len1);
-    printf("xor * inv %s\n", convertBvToStr(multiplyBitVectors(xorBitVectors(vec1, vec2, cells1, cells2), inv_res, cells1, cells2), 1));
-    if (inv_res == NULL) {
-        printf("error3");
-        free(vec1);
-        free(vec2);
-        return 1;
-    }
-    char* inv_str = convertBvToStr(inv_res, cells1);
+    // инверсия
+    inversion(vec1, bits1);
+    char* inv_str = convertBvToStr(vec1, bits1);
     if (inv_str == NULL) {
         printf("error2");
-        free(inv_res);
         free(vec1);
         free(vec2);
         return 1;
     }
-    printf("~    %s\n", inv_str);
+    printf("inversion v1: %s\n", inv_str);
     free(inv_str);
-    free(inv_res);
+
+    // инвертируем обратно для дальнейших тестов
+    inversion(vec1, bits1);
 
     // установка бита 7 в 1
-    int r1 = set_bit_1(vec1, len1, 7);
-    if (r1 != 0) {
-        printf("error13");
-        free(vec1);
-        free(vec2);
-        return 1;
-    }
-    char* set1 = convertBvToStr(vec1, cells1);
-    if (set1 == NULL) {
+    set1(vec1, bits1, 7);
+    char* set1_str = convertBvToStr(vec1, bits1);
+    if (set1_str == NULL) {
         printf("error2");
         free(vec1);
         free(vec2);
         return 1;
     }
-    printf("set1 %s\n", set1);
-    free(set1);
+    printf("set1 bit7: %s\n", set1_str);
+    free(set1_str);
 
-    // сброс бита 0 в 0
-    int r2 = set_bit_0(vec1, len1, 0);
-    if (r2 != 0) {
-        printf("error1");
-        free(vec1);
-        free(vec2);
-        return 1;
-    }
-    char* set0 = convertBvToStr(vec1, cells1);
-    if (set0 == NULL) {
+    // сброс бита 7 в 0
+    set0(vec1, bits1, 7);
+    char* set0_str = convertBvToStr(vec1, bits1);
+    if (set0_str == NULL) {
         printf("error2");
         free(vec1);
         free(vec2);
         return 1;
     }
-    printf("set0 %s\n", set0);
-    free(set0);
+    printf("set0 bit7: %s\n", set0_str);
+    free(set0_str);
 
     free(vec1);
     free(vec2);
@@ -189,45 +166,45 @@ int main() {
     return 0;
 }
 
-unsigned char* convertStrToLongBv(char* str, size_t* cells){
-    if (!(str && cells))
+unsigned char* convertStrToLongBv(char* str, size_t* bits){
+    if (!(str && bits))
         return NULL;
     size_t len = 0, ix = 0;
-    unsigned char mask = 1;          // LSB: начинаем с младшего бита (1)
+    unsigned char mask = 1;
     len = strlen(str);
-    *cells = ((len - 1) / 8) + 1;
-    unsigned char* vec = (unsigned char*)malloc(sizeof(unsigned char) * (*cells));
+    *bits = len;  // сохраняем длину в битах
+    size_t bytes = ((len - 1) / 8) + 1;
+    unsigned char* vec = (unsigned char*)malloc(sizeof(unsigned char) * bytes);
     if (!(vec))
         return NULL;
-    for (size_t i = 0; i < *cells; i++)
+    for (size_t i = 0; i < bytes; i++)
         vec[i] = 0;
-    for (size_t i = 0; i < *cells; i++){
+    for (size_t i = 0; i < bytes; i++){
         for (size_t j = 0; j < 8 && (ix < len); j++){
             if (str[ix] != '0')
-                vec[i] |= mask; // устанавливаем текущий бит
-            mask = mask << 1; // следующий бит старше
+                vec[i] |= mask;
+            mask = mask << 1;
             ix++;
         }
-        mask = 1; // сбрасываем для следующего байта
+        mask = 1;
     }
     return vec;
 }
 
-
-char* convertBvToStr(unsigned char* vec, size_t sz) {
+char* convertBvToStr(unsigned char* vec, size_t bits) {
     if (!vec)
         return NULL;
 
-    size_t bit_count = sz * 8;
-    char* str = (char*)malloc(bit_count + 1);
+    size_t bytes = ((bits - 1) / 8) + 1;
+    char* str = (char*)malloc(bits + 1);
     if (!str)
         return NULL;
 
     size_t str_idx = 0;
 
-    for (size_t byte_idx = 0; byte_idx < sz; byte_idx++) {
+    for (size_t byte_idx = 0; byte_idx < bytes; byte_idx++) {
         unsigned char mask = 1;
-        for (size_t bit = 0; bit < 8; bit++) {
+        for (size_t bit = 0; bit < 8 && str_idx < bits; bit++) {
             if ((vec[byte_idx] & mask) != 0)
                 str[str_idx] = '1';
             else
@@ -237,132 +214,103 @@ char* convertBvToStr(unsigned char* vec, size_t sz) {
         }
     }
 
-    str[bit_count] = '\0';
+    str[bits] = '\0';
     return str;
 }
 
-unsigned char* addBitVectors(unsigned char* vec, unsigned char* vec2, size_t cells, size_t cells2) {
-    if (!vec || !vec2 || (cells != cells2))
+// +
+unsigned char* logSum(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB) {
+    if (!vecA || !vecB || bitsA != bitsB)
         return NULL;
 
-    unsigned char* result = (unsigned char*)malloc(sizeof(unsigned char) * cells);
-    if (!result)
-        return NULL;
-
-    for (size_t i = 0; i < cells; i++) {
-        result[i] = vec[i] | vec2[i];  // побитовое ИЛИ
-    }
-
-    return result;
-}
-
-unsigned char* multiplyBitVectors(unsigned char* vec, unsigned char* vec2, size_t cells, size_t cells2) {
-    if (!vec || !vec2 || (cells != cells2))
-        return NULL;
-
-    unsigned char* result = (unsigned char*)malloc(sizeof(unsigned char) * cells);
-    if (!result)
-        return NULL;
-
-    for (size_t i = 0; i < cells; i++) {
-        result[i] = vec[i] & vec2[i];  // побитовое И
-    }
-
-    return result;
-}
-
-unsigned char* xorBitVectors(unsigned char* vec, unsigned char* vec2, size_t cells, size_t cells2) {
-    if (!vec || !vec2 || (cells != cells2))
-        return NULL;
-
-    unsigned char* result = (unsigned char*)malloc(sizeof(unsigned char) * cells);
-    if (!result)
-        return NULL;
-
-    for (size_t i = 0; i < cells; i++) {
-        result[i] = vec[i] ^ vec2[i];  // побитовое XOR
-    }
-
-    return result;
-}
-
-int set_bit_1(unsigned char *vec, size_t len, size_t k)
-{
-    /*if (!vec || k >= len)
-        return 1;
-    size_t byte = k / 8;
-    size_t bit = k % 8;
-    unsigned char mask = 1;
-    size_t shift = 7 - bit; //01000000 при 2
-
-    mask = mask << shift;
-    vec[byte] = vec[byte] | mask;*/
-    if (!vec || k >= len)
-        return 1;
-    size_t byte = k / 8;
-    size_t bit = k % 8;
-    unsigned char mask = 1 << bit;
-    vec[byte] = vec[byte] | mask;
-    return 0;
-}
-
-int set_bit_0(unsigned char *vec, size_t len, size_t k)
-{
-    /*if (!vec || k >= len)
-        return 1;
-    size_t byte = k / 8;
-    size_t bit = k % 8;
-    unsigned char mask = 1;
-    size_t shift = 7 - bit;
-
-    mask = mask << shift;
-    vec[byte] = vec[byte] & ~mask;*/
-    if (!vec || k >= len)
-        return 1;
-    size_t byte = k / 8;
-    size_t bit = k % 8;
-    unsigned char mask = 1 << bit;
-    vec[byte] = vec[byte] & ~mask;
-    return 0;
-}
-
-unsigned char* inversBitVector(unsigned char* vec, size_t len)
-{
-    if (!vec)
-        return NULL;
-
-    size_t bytes = ((len - 1) / 8) + 1;
-    size_t last_bits = len % 8;
-
-    unsigned char* result = (unsigned char*)malloc(bytes);
+    size_t bytes = ((bitsA - 1) / 8) + 1;
+    unsigned char* result = (unsigned char*)malloc(sizeof(unsigned char) * bytes);
     if (!result)
         return NULL;
 
     for (size_t i = 0; i < bytes; i++) {
-        result[i] = ~vec[i]; // всё инвертируем
-    }
-
-    // Очищаем хвост
-    if (last_bits != 0) {
-        size_t last_byte_idx = bytes - 1; // нужен только последний(там хвост)
-        unsigned char mask = 0;
-        for (size_t j = 0; j < last_bits; j++) {
-            mask = mask | (1 << (7 - j));  // нужна маска где 11..и нужное кол-во нулей 100 010 001 111
-        }
-        result[last_byte_idx] &= mask;
+        result[i] = vecA[i] | vecB[i];
     }
 
     return result;
 }
 
-int shift_left_bit_vector(unsigned char *vec, size_t len, size_t k)
-{
+// &
+unsigned char* logMul(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB) {
+    if (!vecA || !vecB || bitsA != bitsB)
+        return NULL;
 
-    return 0;
+    size_t bytes = ((bitsA - 1) / 8) + 1;
+    unsigned char* result = (unsigned char*)malloc(sizeof(unsigned char) * bytes);
+    if (!result)
+        return NULL;
+
+    for (size_t i = 0; i < bytes; i++) {
+        result[i] = vecA[i] & vecB[i];
+    }
+
+    return result;
 }
 
-int shift_right_bit_vector(unsigned char *vec, size_t len, size_t k)
-{
+// xor
+unsigned char* sumMod2(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB) {
+    if (!vecA || !vecB || bitsA != bitsB)
+        return NULL;
 
-    return 0;
+    size_t bytes = ((bitsA - 1) / 8) + 1;
+    unsigned char* result = (unsigned char*)malloc(sizeof(unsigned char) * bytes);
+    if (!result)
+        return NULL;
+
+    for (size_t i = 0; i < bytes; i++) {
+        result[i] = vecA[i] ^ vecB[i];
+    }
+
+    return result;
+}
+
+void set1(unsigned char *vec, size_t bits, size_t bit) {
+    if (!vec || bit >= bits)
+        return;
+    size_t byte = bit / 8;
+    size_t b = bit % 8;
+    vec[byte] |= (1 << b);
+}
+
+void set0(unsigned char *vec, size_t bits, size_t bit) {
+    if (!vec || bit >= bits)
+        return;
+    size_t byte = bit / 8;
+    size_t b = bit % 8;
+    vec[byte] &= ~(1 << b);
+}
+
+void inversion(unsigned char *vec, size_t bits) {
+    if (!vec)
+        return;
+
+    size_t bytes = ((bits - 1) / 8) + 1;
+    size_t last_bits = bits % 8;
+
+    for (size_t i = 0; i < bytes; i++) {
+        vec[i] = ~vec[i];
+    }
+
+    // очищаем незначащие биты (хвост)
+    if (last_bits != 0) {
+        size_t last_byte_idx = bytes - 1;
+        unsigned char mask = 0;
+        for (size_t j = 0; j < last_bits; j++) {
+            mask |= (1 << j);  // маска младших битов
+        }
+        vec[last_byte_idx] &= mask;
+    }
+}
+
+void shiftLeft(unsigned char *vec, size_t bits, size_t k) {
+
+}
+
+void shiftRight(unsigned char *vec, size_t bits, size_t k) {
+    
 }
